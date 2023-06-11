5dd785435dd757770e916a7e495ba9e0b624a16e
Steeven Montalvo <estywen@gmail.com>
2023-06-11 17:30:08 -0500

    first commit

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,12 @@
+/* alert('Hola Mundo');
+
+let nombre=prompt('Cual es tu nombre','Sin nombre');
+
+console.log(nombre);
+console.log('******'+nombre+'*******'); */
+
+/* const seleccion=confirm('Esta seguro de borrar esto?');
+
+console.log(seleccion); */
+
+console.log(global);
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,72 @@
+
+
+// console.log( console.log('Hola Mundo'));
+
+/* let a=10;
+var b=10;
+const c=10; */
+
+// const = un valor que no cambia
+// c=20; esto no se puede hacer 
+// a=20;
+// b=20;
+
+/* 
+    Diferencia entre let y var    
+*/
+/* 
+    Polyfill
+    es un código qeu provee nuevas funcionalidades en 
+    cogido antiguo de javascript
+*/
+
+/* 
+let a=10,
+    b=20,
+    c=30,
+    d=10, 
+    x=a+b; */
+
+/* 
+console.log(x);
+console.warn(x); // nos permite mostrar una advertencia
+console.error(x); nos permite mostrar un error 
+*/
+/* console.log('%c Mis variables','color:blue; font-weight:bold');
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log('a',a);
+console.log('b',b);
+console.log('c',c);
+
+console.log({a});
+console.log({b});
+console.log({c}); */
+
+/* alert('Hola desde app.js') */
+let a=10,
+    b=20,
+    c='Hola ',
+    d='Spiderman', 
+    x=a+b;
+
+const saludo= c+d;
+/* console.log(saludo); */
+
+/* console.table({a,b,c,d,x});  permite mostrar el código en una tabla*/
+
+c='Hola de nuevo';
+
+// Nuca declarar con var ya que se sobrepone sobre algunas variables en JavaScript
+/* let outerWidth=10000;
+const outerHeight=10000; */
+
+/* 
+    Declarar let me permite un codigo mas limpio 
+    y  nos permite mostrar los errores.
+*/
+let miNuevoNombre='Steven Montalvo';
+console.log(miNuevoNombre);
